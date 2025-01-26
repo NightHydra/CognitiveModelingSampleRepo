125b652695935912cf7d6aedd7a157254a53062a
Alek Krupka <krupka@rpi.edu>
2025-01-25 23:33:38 -0500

    Removed hardcoded values for the simulator

--- seir/simulation/simulator.py
+++ seir/simulation/simulator.py
@@ -1,28 +1,28 @@
 
-def simulate_seir(parameters, init_conditions, days=51):
+def simulate_seir(parameters, init_conditions, first_day = 1, last_day=51):
     """TODO"""
 
     # Extract parameters and initial conditions (Your code here)
     beta, sigma, gamma = parameters
     S0, E0, I0, R0 = init_conditions
     N = S0 + E0 + I0 + R0
-    S = [S0] + [None] * (days-1)
-    E = [E0] + [None] * (days-1)
-    I = [I0] + [None] * (days-1)
-    R = [R0] + [None] * (days-1)
+    S = [S0] + [None] * (last_day - first_day)
+    E = [E0] + [None] * (last_day - first_day)
+    I = [I0] + [None] * (last_day - first_day)
+    R = [R0] + [None] * (last_day - first_day)
     
     # For each day, perform SEIR update
-    for t in range(1, days):
+    for t in range(first_day, last_day):
 
         # Compute new cases and update equations
-        E_new = (beta*S[t-1]*I[t-1]) / N
-        I_new = sigma*E[t-1]
-        R_new = gamma*I[t-1]
+        E_new = (beta*S[t-first_day]*I[t-first_day]) / N
+        I_new = sigma*E[t-first_day]
+        R_new = gamma*I[t-first_day]
       
         # Update equations
-        S[t] = S[t-1] - E_new
-        E[t] = E[t-1] + E_new - I_new
-        I[t] = I[t-1] + I_new - R_new
-        R[t] = R[t-1] + R_new
+        S[t] = S[t-first_day] - E_new
+        E[t] = E[t-first_day] + E_new - I_new
+        I[t] = I[t-first_day] + I_new - R_new
+        R[t] = R[t-first_day] + R_new
 
     return (S, E, I, R)
