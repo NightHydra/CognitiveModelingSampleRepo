d3dcaff4df0bd88463e15656b8637c416d35cc93
Alek Krupka <krupka@rpi.edu>
2025-01-26 20:55:46 -0500

    Removed hardcoded variables

--- seir/viz/plot_seir.py
+++ seir/viz/plot_seir.py
@@ -1,15 +1,19 @@
 import matplotlib.pyplot as plt
 
 
-def plot_results(infected : list[float], day_fontsize : int = 16, infected_cases_fontsize : int = 16, 
-    outbreak_test_fontsize : int = 20, axis_brightness : float = 0.2 ):
+def plot_results(infected : list, day_fontsize : int = 16, infected_cases_fontsize : int = 16, 
+    outbreak_test_fontsize : int = 20, axis_brightness : float = 0.2 , x_axis_label_name : str = 'Day' ,
+    y_axis_label_name : str = "Number of Infected Cases",  plot_title : str = "Simulated Outbreak",
+    plot_color : str = '#AA0000', plot_line_style : str = 'dashed', data_marker : str = 'o',
+    plot_width : float = 10, plot_height : float = 6
+    ):
     
     """Plots the time series of infected cases."""
     
-    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
-    ax.plot(infected, color='#AA0000', linestyle='dashed', marker='o')
-    ax.set_xlabel('Day', fontsize=day_fontsize)
-    ax.set_ylabel('Number of Infected Cases', fontsize=infected_cases_fontsize)
-    ax.set_title('Simulated Oubreak', fontsize=outbreak_test_fontsize)
+    fig, ax = plt.subplots(figsize=(plot_width, plot_height))
+    ax.plot(infected, color=plot_color, linestyle=plot_line_style, marker=data_marker)
+    ax.set_xlabel(x_axis_label_name, fontsize=day_fontsize)
+    ax.set_ylabel(y_axis_label_name, fontsize=infected_cases_fontsize)
+    ax.set_title(plot_title, fontsize=outbreak_test_fontsize)
     ax.grid(alpha=axis_brightness)
     return fig
